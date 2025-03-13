d472f6e46a04fc8ca18d403f6fa070e4292656b1
ranjith2007 <ranjithkumar0799@gmail.com>
2025-03-13 11:54:03 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,8 @@
 module "ec2" {
   source = "../terraform-aws-ec2"
-  
+  # these are variables expected module, not arguments of resource definition
+  # You can also create variables or else you can directly use values here
+  ami_id = var.ami_something_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0f575d808e73f7ee2"]
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "ami_something_id" {
+  default = "ami-09c813fb71547fc4f"
+}
